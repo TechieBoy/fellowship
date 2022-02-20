4ce87eacbbb090004b6f08a4c962dacd1cce3938
Sanket Shanbhag <techieboy.teknas@gmail.com>
2022-02-20 15:32:00 +0530

    Week2 exercise fixes

---
+++ week2/exercise/voting/README.md
@@ -0,0 +1,3 @@
+# Solana implementation for [voting/delegation contract](https://docs.soliditylang.org/en/v0.8.11/solidity-by-example.html)
+
+Run the tests using `anchor test` after setting the appropriate program key in Anchor.toml and lib.rs
--- week2/exercise/voting/tests/voting.ts
+++ week2/exercise/voting/tests/voting.ts
@@ -12,13 +12,17 @@ describe('voting', () => {
   const program = anchor.workspace.Voting as Program<Voting>;
   const admin = anchor.web3.Keypair.generate();
   const adminAccount = anchor.web3.Keypair.generate();
+
   const voter1 = anchor.web3.Keypair.generate();
   const voter1account = anchor.web3.Keypair.generate();
+
   const voter2 = anchor.web3.Keypair.generate();
   const voter2account = anchor.web3.Keypair.generate();
-  const proposal = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
+  const proposal = anchor.web3.Keypair.generate();
+  const proposal2 = anchor.web3.Keypair.generate();
+
+  it('Initialize contract!', async () => {
     await provider.connection.confirmTransaction(
       await provider.connection.requestAirdrop(admin.publicKey, 1 * anchor.web3.LAMPORTS_PER_SOL), "confirmed");
     const tx = await program.rpc.initialize(
@@ -36,7 +40,7 @@ describe('voting', () => {
     console.log("Init transaction signature", tx);
   });
 
-  it('Create proposal!', async () => {
+  it('Create 2 proposals!', async () => {
     const [proposal_pda, _bump] = await anchor.web3.PublicKey.findProgramAddress(
       [Buffer.from(anchor.utils.bytes.utf8.encode("proposal"))],
       program.programId
@@ -52,6 +56,20 @@ describe('voting', () => {
           systemProgram: anchor.web3.SystemProgram.programId,
         },
         signers: [admin, proposal]
+      },
+    );
+
+    const tx2 = await program.rpc.addNewProposal(
+      2,
+      {
+        accounts: {
+          adminKey: admin.publicKey,
+          proposalPda: proposal_pda,
+          adminAccount: adminAccount.publicKey,
+          proposalAccount: proposal2.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [admin, proposal2]
       },
     );
 
@@ -104,24 +122,42 @@ describe('voting', () => {
 
 
    });
-  it('Vote with first voter!', async () => {
-    const proposalOnchaino = await program.account.voterAccount.fetch(proposal.publicKey);
-    console.log(proposalOnchaino);
+
+  it('Vote with first voter to proposal 2!', async () => {
     const tx = await program.rpc.vote(
       {
         accounts: {
           voterKey: voter1.publicKey,
           voteAccount: voter1account.publicKey,
-          proposalAccount: proposal.publicKey,
+          proposalAccount: proposal2.publicKey,
         },
         signers: [voter1]
       }
     );
     const voter1onchain = await program.account.voterAccount.fetch(voter1account.publicKey);
-    const proposalOnchain = await program.account.voterAccount.fetch(proposal.publicKey);
+    const proposalOnchain = await program.account.proposalAccount.fetch(proposal2.publicKey);
     assert.ok(voter1onchain.voted === true);
     assert.ok(proposalOnchain.votes.toNumber() === 30);
+   });
 
-
+  it('Find the winning proposal!', async () => {
+    let proposals = [proposal.publicKey, proposal2.publicKey];
+    const [proposal_pda, _bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("proposal"))],
+      program.programId
+    );
+    let max_votes = -1;
+    let winner = null;
+    for (let p of proposals) {
+      const info = await provider.connection.getAccountInfo(p);
+      // Assert proper owner
+      assert.ok(info.owner.equals(program.programId));
+      let paccount =  await program.account.proposalAccount.fetch(p);
+      if (paccount.votes.toNumber() > max_votes) {
+        max_votes = paccount.votes.toNumber();
+        winner = paccount.id;
+      }
+    }
+    console.log("Winner is proposal #", winner);
    });
 });
